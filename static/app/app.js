2febab2dbdf3aa5e2337b1a34b75e445596764d7
Simon Kusterer <simon@soped.com>
2013-01-05 12:41:33 +0100

    some clientside improvements

--- static/app/app.js
+++ static/app/app.js
@@ -1,4 +1,5 @@
-(function($, B, _, win) {
+(function ($, Backbone, _, win, alertify) {
+  'use strict';
 
   // This will be the entrypoint to our App
   var App = {};
@@ -10,77 +11,38 @@
   // Main App View
   App.MainView = Backbone.View.extend({
 
-    initialize: function() {
+    initialize:function () {
       this.user = new App.UserModel();
-      this.collection = new App.TaskCollection();
+      this.tasks = new App.TaskCollection();
 
-      this.loginView = new App.LoginView({ el: this.$('#signup'), model: this.user });
-      this.addView   = new App.AddTaskView({ el: this.$('#add'), collection: this.collection, model: this.user });
-      this.listView  = new App.TaskListView({ el: this.$('#list'), collection: this.collection });
-    }
-
-  });
-
-  // The Login Form
-  App.LoginView = Backbone.View.extend({
-
-    events: {
-      'submit': 'login'
-    },
-
-    initialize: function() {
-      this.model.on('change:hash', this.hide, this);
-    },
-
-    login: function(ev) {
-      ev.preventDefault();
-      App.utils.rpcRequest(
-        'generateHash',
-        [],
-        {
-          fe_username: this.$('#username').val(),
-          fe_password: this.$('#password').val()
-        },
-        _.bind(function(data) {
-          if (data.error) {
-              alertify.error(data.error.message);
-          } else {
-            this.model.set('hash', data.result);
-            App.TaskModel.prototype.postParams = {'fe_hash': data.result};
-            App.TaskCollection.prototype.postParams = {'fe_hash': data.result};
-            alertify.success('Login succeeded');
-          }
-        }, this)
-      );
-    },
-
-    hide: function() {
-      this.$el.fadeOut();
+      this.loginView = new App.LoginView({ el:this.$('#signup'), model:this.user });
+      this.addView = new App.TaskAddView({ el:this.$('#add'), collection:this.tasks, model:this.user });
+      this.listView = new App.TaskListView({ el:this.$('#list'), collection:this.tasks });
     }
 
   });
 
   // Add Task Form
-  App.AddTaskView = Backbone.View.extend({
+  App.TaskAddView = Backbone.View.extend({
 
-    events: {
-      'submit': 'add'
+    events:{
+      'submit':'add'
     },
 
-    initialize: function() {
+    initialize:function () {
       this.model.on('change:hash', this.show, this);
     },
 
-    add: function(ev) {
+    add:function (ev) {
       ev.preventDefault();
-      var task = new App.TaskModel();
-      task.set('title', this.$('#title').val());
-      task.set('done', false);
+      var task = new App.TaskModel({
+        'title':this.$('#title').val()
+      });
       task.save();
       this.collection.add(task);
     },
 
-    show: function() {
+    show:function () {
       this.$el.fadeIn();
     }
 
@@ -89,25 +51,27 @@
   // The entire Task List
   App.TaskListView = Backbone.View.extend({
 
-    initialize: function() {
+    initialize:function () {
       this.collection.on('reset', this.render, this);
       this.collection.on('add', this.add, this);
       this.collection.fetch();
     },
 
-    render: function() {
+    render:function () {
       // reset list
       this.$el.empty();
 
       // append list items
-      this.collection.forEach(function(task) {
-        this.add(task);
-      }, this);
+      this.collection.forEach(this.append, this);
     },
 
     add: function(task) {
       alertify.log('Added Task: ' + task.get('title'));
-      this.$el.append(new App.TaskItemView({ model: task }).render().el);
+      this.append(task);
+    },
+
+    append:function (task) {
+      this.$el.append(new App.TaskItemView({ model:task }).render().el);
     }
 
   });
@@ -115,42 +79,76 @@
   // Represents one Row in the Task List
   App.TaskItemView = Backbone.View.extend({
 
-    tagName: 'tr',
+    tagName:'tr',
 
-    events: {
-      'click .remove': 'remove',
-      'click .done': 'done'
+    events:{
+      'click .remove':'destroy',
+      'click .done':'done'
     },
 
-    template: _.template($("#task-item").html()),
+    template:_.template($("#task-item").html()),
 
-    initialize: function() {
-      App.main.user.on('change:hash', this.showControls, this);
+    initialize:function () {
+      App.main.user.on('change:hash', this.render, this);
+      this.model.on('change:done', this.render, this);
     },
 
-    render: function() {
+    render:function () {
       this.$el.html(this.template(this.model.toJSON()));
-      if (App.main.user.get('hash')) {
-        this.showControls();
-      }
+      this.$el.toggleClass('done', this.model.get('done'));
+      App.main.user.get('hash') && this.$('.controls').show();
       return this;
     },
 
-    remove: function() {
-      //this.remove();
+    destroy:function (ev) {
+      ev.preventDefault();
       alertify.log('Deleted Task: ' + this.model.get('title'));
       this.model.destroy();
-      this.$el.remove();
+      this.remove();
     },
 
-    done: function(ev) {
+    done:function (ev) {
       ev.preventDefault();
       this.model.set('done', !this.model.get('done'));
       this.model.save();
+    }
+
+  });
+
+
+  // The Login Form
+  App.LoginView = Backbone.View.extend({
+
+    events:{
+      'submit':'login'
     },
 
-    showControls: function() {
-      this.$('.controls').show();
+    initialize:function () {
+      this.model.on('change:hash', this.hide, this);
+    },
+
+    login:function (ev) {
+      ev.preventDefault();
+      App.utils.rpcRequest(
+        'generateHash',
+        {
+          fe_username:this.$('#username').val(),
+          fe_password:this.$('#password').val()
+        }
+      ).success(_.bind(function (data) {
+        if (data.error) {
+          alertify.error(data.error.message);
+        } else {
+          this.model.set('hash', data.result);
+          App.TaskModel.prototype.postParams = {'fe_hash':data.result};
+          App.TaskCollection.prototype.postParams = {'fe_hash':data.result};
+          alertify.success('Login succeeded');
+        }
+      }, this));
+    },
+
+    hide:function () {
+      this.$el.fadeOut();
     }
 
   });
@@ -161,21 +159,25 @@
 
   App.UserModel = Backbone.Model.extend({
 
-    defaults: {
-      status: ''
+    defaults:{
+      hash:false
     }
 
   });
 
   App.TaskModel = Backbone.Model.extend({
 
-    url: '/rpc.php',
+    url:'/rpc.php',
 
-    methods: {
-      read:   'Todo.retrieve',
-      create: 'Todo.create',
-      delete: 'Todo.delete',
-      update: 'Todo.update'
+    methods:{
+      'read':'Todo.retrieve',
+      'create':'Todo.create',
+      'delete':'Todo.delete',
+      'update':'Todo.update'
+    },
+
+    defaults:{
+      done:false
     }
 
   });
@@ -186,53 +188,51 @@
 
   App.TaskCollection = Backbone.Collection.extend({
 
-    url: '/rpc.php',
+    url:'/rpc.php',
 
-    model: App.TaskModel,
+    model:App.TaskModel,
 
-    methods: {
-      read:   'Todo.retrieve'
+    methods:{
+      read:'Todo.retrieve'
     }
 
   });
 
   App.utils = {
 
-    rpcRequest: function(method, rpcParams, postParams, success, error) {
+    rpcRequest:function (method, postParams, rpcParams) {
 
       var data = {
-        rpc: JSON.stringify({
-          jsonrpc : '2.0',
-          method  : method,
-          id      : _.uniqueId(),
-          params  : rpcParams
+        rpc:JSON.stringify({
+          jsonrpc:'2.0',
+          method:method,
+          id:_.uniqueId(),
+          params: rpcParams || {}
         }),
-        provider: 'json'
+        provider:'json'
       };
 
       _.defaults(data, postParams);
 
       return $.ajax({
-        url: '/rpc.php',
-        type: 'POST',
-        dataType: 'json',
-        data: data,
-        success: success,
-        error: error
+        url:'/rpc.php',
+        type:'POST',
+        dataType:'json',
+        data:data
       });
     }
 
   };
 
   // Bootstrap
-  $(function() {
+  $(function () {
 
     // Initialize the main View
-    App.main = new App.MainView({ el: $('#app') });
+    App.main = new App.MainView({ el:$('#app') });
 
     // Export App
-    window.App = App;
+    win.App = App;
 
   });
 
-})(jQuery, Backbone, _, window);
+})(jQuery, Backbone, _, window, alertify);
